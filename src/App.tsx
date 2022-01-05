4c84ecb7e4d2548f2a2d56f879a9bfaa99c066c6
cyrus <mdoaie@yahoo.co.uk>
2022-01-05 18:05:02 +0330

    added api test

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,40 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const [state, setState] = React.useState({
+    post: "",
+    response: "",
+    responseToPost: "",
+  });
+
+  const handleSubmit = React.useCallback(async () => {
+    const response = await fetch("/api/messages", {
+      body: JSON.stringify({ post: state.post }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    });
+    const body = await response.text();
+    setState((s) => ({ ...s, responseToPost: body }));
+  }, [state]);
+
+  const callApi = React.useCallback(async () => {
+    const response = await fetch("/api/users/1234");
+    const body = await response.json();
+    if (response.status !== 200) {
+      throw Error(body.message);
+    }
+    return body;
+  }, []);
+
+  React.useEffect(() => {
+    callApi()
+      .then((res) => setState((s) => ({ ...s, response: res.express })))
+      // tslint:disable-next-line:no-console
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +53,22 @@ function App() {
           Learn React
         </a>
       </header>
+      <p>{state.response}</p>
+
+      <p>
+        <strong>Post to Server:</strong>
+      </p>
+      <input
+        type="text"
+        value={state.post}
+        // tslint:disable-next-line jsx-no-lambda
+        onChange={(e) => setState((s) => ({ ...s, post: e.target.value }))}
+      />
+      <button type="submit" onClick={handleSubmit}>
+        Submit
+      </button>
+
+      <p>{state.responseToPost}</p>
     </div>
   );
 }
